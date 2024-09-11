f23ba788263547c9e600388d3f17a1356f136fa9
JoeChou0 <ooss1.tw@yahoo.com.tw>
2024-09-11 17:18:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var ajaxCall = (key, url, prompt) => {
                             "content": prompt 
                         }
                 	],
-                	"max_tokens": 900,
+                	"max_tokens": 1500,
                 	"n": 1,
                 	"top_p": 0.95,
                 	"temperature": 1.0
